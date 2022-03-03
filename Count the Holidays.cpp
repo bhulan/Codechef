35173ce09b2d3ba6c738fc50963a28c4e7b5127f
Bhulan Deb <94342258+bhulan@users.noreply.github.com>
2022-03-03 13:58:13 +0530

    SUNDAY

---
+++ Count the Holidays.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define mp make_pair
+#define el "\n"
+#define pb push_back
+//#define sort(v) sort(s.begin(),s.end())
+#define vpii vector<pair<int, int>>
+#define mytestcase long long int t; cin>>t;while(t--)
+#define vi vector<int>
+#define vll vector<ll>
+#define ll long long
+#define all(a) (a).begin(), (a).end()
+#define vpii vector<pair<int, int>>
+
+using namespace std;
+    int main(){
+     //Lets do
+     mytestcase
+     {
+     int n;
+     cin>>n;
+     int ans=8;
+      int a[n];
+      for(int i=0;i<n;i++){
+      	cin>>a[i];
+      	if(a[i]==6 || a[i]==7 || a[i]==13 || a[i]==14 || a[i]==20 || a[i]==21 || a[i]==27 || a[i]==28)
+      	ans;
+      	else
+      	ans++;
+	  }
+	  cout<<ans<<el;
+ }
+}
+	  	
+	
+
+
+		
+
+
+
+
+
+
